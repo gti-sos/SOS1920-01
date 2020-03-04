6b84c1e9f330e1b55be55c2f03464f755be96bb6
Escobar1993 <antonioescobar070@gmail.com>
2020-03-04 10:30:35 +0000

    Ejercicio mostrar hora con '/time'

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ var app = express();
 var time = require("time");
 var now = new time.Date();
 
-now.setTimezone("America/Los_Angeles");
+
+now.setTimezone("UTC-1");
 
 app.get("/time",(request,response) => {
-	response.send("<html>"+time+"</html>");
+	response.send("<html>"+now.toString()+"</html>");
 });
 
 app.listen(80);
