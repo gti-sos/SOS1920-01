12828ab94c64469355035fa913dcfec2c0ad24be
escobar1993 <antonioescobar070@gmail.com>
2020-05-04 15:13:26 +0000

    v2

--- api-escobar/index.js
+++ api-escobar/index.js
@@ -2,10 +2,10 @@ var apiV1 = require("./v1");
 
 var apiV2 = require("./v2");
 
-module.exports = function(app,BASE_PATH){
-	apiV1(app,BASE_PATH+"/v1");
+module.exports = function(app1,BASE_PATH){
+	apiV1(app1,BASE_PATH+"/v1");
 }
 
-module.exports = function(app,BASE_PATH){
-	apiV2(app,BASE_PATH+"/v2");
+module.exports = function(app2,BASE_PATH){
+	apiV2(app2,BASE_PATH+"/v2");
 }
