6c8b54fd6b79c3b2ffab28f50e7e45eb7dd3a3a1
juanfran94 <juanfran.lainez.94@gmail.com>
2020-05-06 18:44:55 +0200

    cambios en back

--- index.js
+++ index.js
@@ -4,12 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const BASE_PATH = "/api";
 
-
 const port = process.env.PORT || 9999;
-
-
-//const natalitydb = require(path.join(__dirname, "api-juanfran"));//el método join permite unir un directorio con un archivo.
-
 
 app.use("/",express.static("./public")); 
 app.use(bodyParser.json());
@@ -20,23 +15,19 @@ app.get("/public",(request,response) => {
 
 
 //////////////////// Antonio Escobar Núñez ////////////////////
-var apiEscobarv1 = require("./src/back/api-escobar/v1");
-var apiEscobarv2 = require("./src/back/api-escobar/v2");
-apiEscobarv1(app,BASE_PATH);
-apiEscobarv2(app,BASE_PATH);
+var apiEscobar = require("./src/back/api-escobar");
+apiEscobar(app,BASE_PATH);
+
 
 
 /////////////////// API JUANFRAN //////////////
-var apijuanfranv1 = require("./src/back/api-juanfran/v1");
-var apijuanfranv2 = require("./src/back/api-juanfran/v2");
-apijuanfranv1(app, BASE_PATH);
-apijuanfranv2(app, BASE_PATH);
+var apijuanfran = require("./src/back/api-juanfran");
+apijuanfran(app, BASE_PATH);
+
 
 /////////////////// API ANGELA //////////////
-var api_angelav1 = require("./src/back/api-angela/v1");
-var api_angelav2 = require("./src/back/api-angela/v2");
-api_angelav1(app, BASE_PATH);
-api_angelav2(app, BASE_PATH);
+var api_angela = require("./src/back/api-angela");
+api_angela(app, BASE_PATH);
 
 //////////////////// Servidor ////////////////////
 
