0a3a6541ab272508ee0ad6f56fb8a91de37bf1df
angtorcal <angela.torcal93@gmail.com>
2020-05-07 00:01:06 +0200

    front

--- src/front/App.svelte
+++ src/front/App.svelte
@@ -1,8 +1,28 @@
 <script>
     import Router from 'svelte-spa-router';
-    import Home
+    import Home from './Home.svelte';
+
+// API JUANFRAN
+
+// API ANGELA
+    import PovertyTable from './povertyApi/PovertyTable.svelte';
+    import EditNPoverty from './povertyApi/EditNPoverty.svelte';
+
+// API ESCOBAR
+
+    const routes = {
+		"/": Home,
+	// JUANFRAN
+
+    // ANGELA
+		"/poverty-stats": PovertyTable,
+		"/poverty-stats/:country/:year": EditPoverty,
+	// ESCOBAR	
+		
+		"*": NotFound
+	};
 </script>
 
 <main>
-
+    <Router {routes} />
 </main>
