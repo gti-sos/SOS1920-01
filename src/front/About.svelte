d8cacb5f79f85d7bc3ffc4c5ae43a57570b3d2de
juanfran94 <juanfran.lainez.94@gmail.com>
2020-06-04 00:40:47 +0200

    video

---
+++ src/front/About.svelte
@@ -0,0 +1,52 @@
+<script>
+	import Button from "sveltestrap/src/Button.svelte";
+    import {
+        pop
+    } from "svelte-spa-router";
+	
+</script>
+
+<main>
+  <h1 class="display-4" style="text-align: center;" >  </h1>
+      
+	<Button outline color="secondary" on:click="{pop}"><i class="fas fa-arrow-circle-left"></i> </Button>
+  <p></p>
+  <div class="card-deck">
+        <div class="card">
+            <div class="embed-responsive embed-responsive-16by9 video">
+              <iframe width="816" height="441" src="" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+          <div class="card-body">
+            <h5 class="card-title">Natalidad</h5>
+            <p class="card-text">Vídeo explicativo sobre datos de natalidad en varios países.</p>
+          </div>
+        </div>
+
+        <div class="card">
+            <div class="embed-responsive embed-responsive-16by9 video">
+                <iframe width="900" height="506" src="" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+          <div class="card-body">
+            <h5 class="card-title">Pobreza</h5>
+            <p class="card-text">Vídeo explicativo sobre los riegos de pobreza en varios países.</p>
+          </div>
+        </div>
+
+        <div class="card">
+            <div class="embed-responsive embed-responsive-16by9 video">
+              <iframe width="757" height="409" src="" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+          <div class="card-body">
+            <h5 class="card-title">Emigración</h5>
+            <p class="card-text">Vídeo explicativo sobre la emigración en varios países.</p>
+          </div>
+        </div>
+    </div>
+    
+    
+    
+    
+
+
+
+</main>
